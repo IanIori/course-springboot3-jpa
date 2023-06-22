a34b1eae0781be5e4ac869076aea2484b65de8de
Ian Iori <ianmessia@gmail.com>
2023-06-21 21:17:05 -0300

    Category entity

--- src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
+++ src/main/java/com/educandoweb/course/repositories/CategoryRepository.java
@@ -2,9 +2,9 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.Category;
 
 //JpaRepository already has methods to implement as an interfaceand inherits notation
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
